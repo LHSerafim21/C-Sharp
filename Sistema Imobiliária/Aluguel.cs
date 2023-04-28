aab02cbcbd7dfc07c9a05b28ff78f67167330bc6
Luiz Henrique Serafim <luizhserafim2001@hotmail.com>
2023-04-28 20:52:19 -0300

    Implementação de Getrs e Setrs na classe aluguel

--- Sistema Imobiliária/Aluguel.cs
+++ Sistema Imobiliária/Aluguel.cs
@@ -9,16 +9,79 @@ namespace Sistema_Imobiliária
     public class Aluguel
     {
         private int CodigoAluguel;
-        private Cliente Cliente;
-        private Corretor Corretor;
-        private Imovel Imovel;
+        private Cliente cliente;
+        private Corretor corretor;
+        private Imovel imovel;
         private DateTime DataAluguel;
         private DateTime DataDevolucao;
         private DateTime DataPagementoMensal;
         private float ValorPagementoMensal;
         private Pagamento FormaPagamento;
         private ArrayList<Seguro> SegurosContratados;
+        private Boolean pago;
 
+        public Aluguel(int CodigoAluguel, Cliente cliente, Corretor corretor, Imovel imovel, DateTime DataAluguel, DateTime DataDevolucao, DateTime DataPagamentoMensal, float ValorPagementoMensal, Pagamento FormaPagamento, ArrayList<Seguro> SegurosContratados, Boolean pago){
+            this.CodigoAluguel = CodigoAluguel;
+            this.cliente = cliente;
+            this.corretor = corretor;
+            this.imovel = imovel;
+            this.DataAluguel = DataAluguel;
+            this.DataDevolucao = DataDevolucao;
+            this.DataPagementoMensal = DataPagementoMensal;
+            this.ValorPagementoMensal = ValorPagementoMensal;
+            this.FormaPagamento = FormaPagamento;
+            this.SegurosContratados = SegurosContratados;
+            this.pago = pago;
+        }
+
+        public int getCodigoAluguel(){
+            return CodigoAluguel;
+        } 
+        public void setCodigoAluguel(int CodigoAluguel){
+            this.CodigoAluguel = CodigoAluguel;
+        }
+        public Cliente getCliente(){
+            return cliente;
+        }
+        public void setCliente(Cliente cliente){
+            this.cliente = cliente;
+        }
+        public Corretor getCorretor(){
+            return corretor;
+        }
+        public void setCorretor(Corretor corretor){
+            this.corretor = corretor;
+        }
+        public Imovel getImovel(){
+            return imovel;
+        }
+        public void setImovel(Imovel imovel){
+            this.imovel = imovel;
+        }
+        public DateTime getDataAluguel(){
+            return DataAluguel;
+        }
+        public void setDataAluguel(){
+            this.DataAluguel = DataAluguel;
+        }
+        public DateTime getDataDevolucao(){
+            return DataDevolucao;
+        }
+        public void setDataDevolucao(DateTime DataDevolucao){
+            this.DataDevolucao = DataDevolucao;
+        }
+        public DateTime getDataPagamentoMensal(){
+            return DataPagementoMensal;
+        }
+        public void 
+
+
+
+
+
+
+
+        
         
     }
 }
